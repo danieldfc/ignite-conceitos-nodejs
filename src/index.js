63398df101f803d57ffa8249bab54dbfd38e7046
Daniel Felizardo <daniel.david772@gmail.com>
2021-03-09 14:02:55 -0300

    feat: delete todo

--- src/index.js
+++ src/index.js
@@ -99,7 +99,18 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { id } = request.params;
+  const { user } = request;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+
+  if (todoIndex < 0) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  user.todos.splice(todoIndex, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
